fdf3ee7d9ed9e88f31acc0be584b7dacf9cec2d0
MANOJ SINGH ADHIKARI <mnjups30@gmail.com>
2019-07-14 21:55:41 +0530

    Basic cmath library functions

---
+++ C++Progs/cmath.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //pow, ceil, floor, abs, cos, sin, tan, acos, asin, atan
+
+    double PI=3.14159265;
+    double angle=0;
+
+    cout<<pow(2,4)<<endl;
+    cout<<sqrt(25)<<endl;
+    cout<<ceil(1.5)<<endl;
+    cout<<floor(1.5)<<endl;
+    cout<<abs(-12)<<endl;
+    cout<<cos(angle*PI/100.0)<<endl;
+
+    return 0;
+}
